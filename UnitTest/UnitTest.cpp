284860ae61f4511dfdc53737c74615a5e5da9b63
Demian Rymar <ua.demyan@gmail.com>
2024-04-11 22:06:11 +0300

    Done

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -1,5 +1,6 @@
 ﻿#include "pch.h"
 #include "CppUnitTest.h"
+#include "../LAB_11.2/Main.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -8,9 +9,38 @@ namespace UnitTest
 	TEST_CLASS(UnitTest)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(TestCalculateAverageGrade)
 		{
+			// Arrange
+			Student student;
+			student.physics_grade = 4;
+			student.math_grade = 5;
+			student.additional_grade.programming = 3;
+
+			// Act
+			double result = CalculateAverageGrade(student);
+
+			// Assert
+			Assert::AreEqual(4.0, result);
 		}
+
+		TEST_METHOD(TestCalculatePercentage)
+		{
+			// Arrange
+			const int N = 3;
+			Student students[N];
+
+			students[0].physics_grade = 5;
+			students[1].physics_grade = 4;
+			students[2].physics_grade = 3;
+
+			// Act
+			double result = CalculatePercentage(students, N);
+
+			// Assert
+			Assert::AreEqual(66.666, result, 0.001); 
+		}
+
 	};
 }
